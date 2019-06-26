8423a485fff2f5fd674db2745f2057854a582d05
Daria Lykova <daria.lykova@gmail.com>
2019-06-26 21:56:35 +0200

    Creating About & Project files/Making a few practical modifications to header,footer, hello & index files

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <div class="border-t border-smoke px-8 py-4">
-            <div class="flex justify-center text-grey">
-                Made with React
+        <div className="footer-wrapper">
+                <div className="footer-content">
+                        Made with React
                 </div>
         </div>
-    )
+)
 
 }
 
--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import Footer from "./Footer"
 import './css/tailwind.css'
 
 const App = () => {
-    return (
-        <div className="min-h-screen">
-            <div className="flex flex-col w-full fixed pin-l pin-y">
-                <Header />
-                <div className="px-4 pt-4 flex-1 overflow-y-scrol">
-                    <Hello />
-                </div>
-                <Footer />
-            </div >
+    return (<div className="page-wrapper">
+        <div className="page-content">
+            <Header />
+            <div className="content-wrapper">
+                <Hello />
+            </div>
+            <Footer />
         </div >
+    </div >
+
     )
 };
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(< App />, document.getElementById("app"))
