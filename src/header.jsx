967a4cd40ace8cbb7c00f470fbc6cc2ebd1abd1f
Daria Lykova <daria.lykova@gmail.com>
2019-06-26 17:16:07 +0200

    Incorporating tailwind into the project for styling/Making first visual styling modifications

---
+++ src/header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-blue-darkest p-6">
+            <h1 className="text-white text-3xl">My Portfolio</h1>
+        </nav>
+    )
+}
+
+export default Header
