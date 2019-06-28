c99b585fea916062fc362a40f460ccb93b968533
Daria Lykova <daria.lykova@gmail.com>
2019-06-28 16:11:04 +0200

    Creating page, name & home divs for styling/Deleting the illustrations

--- src/About.jsx
+++ src/About.jsx
@@ -7,7 +7,6 @@ const About = () => {
         <div className="content-wrapper">
             <div className="flex flex-wrap mb-4">
                 <div className="w-full lg:w-1/4 md:w-1/2">
-                    <UndrawDesignerLife />
                 </div>
                 <div className="w-full lg:w-3/4 md:w-1/2">
                     <h1 className="content-text">About Me</h1>
--- src/header.jsx
+++ src/header.jsx
@@ -4,43 +4,59 @@ import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div style={topDiv} >
-           <div style={nameDiv} className="text-white hover:text-grey no-underline">Daria Lykova</div> 
+        <div style={pageDiv}>
+            <div style={nameDiv} className="text-white hover:text-grey no-underline">DARIA LYKOVA</div>
             <nav>
-                <h1>
-                    <Link className="text-white hover:text-grey no-underline" to='/'>HOME</Link>
-                </h1>
+                <div style={homeDiv}>
+                    <h1>
+                        <Link className="text-white hover:text-grey no-underline" to='/'>HOME</Link>
+                    </h1>
+                </div>
                 <ul className="list-reset flex">
-                    <li><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About</NavLink></li>
-                    <li><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>Projects</NavLink></li>
+                    <li><NavLink className="text-white hover:text-grey no-underline" activeStyle={{ fontWeight: "bold" }} to='/about'>About</NavLink></li>
+                    <li><NavLink className="text-white hover:text-grey no-underline" activeStyle={{ fontWeight: "bold" }} to='/projects'>Projects</NavLink></li>
                 </ul>
             </nav>
         </div>
     )
 }
 
-const topDiv = {
+const pageDiv = {
     height: '100vh',
-    width: '270px',
+    width: '150%',
     textAlign: 'left',
     paddingTop: '55px',
     background: '#E5E5E5',
     fontSize: '50px',
-    //backgroundImage: `url(${light})`
 }
 
 const nameDiv = {
-position: 'absolute',
-width: '268px',
-height: '127px',
-fontFamily: 'Monserrat',
-fontStyle: 'normal',
-fontWeight: 'normal',
-fontSize: '72px',
-lineHeight: '81.2%',
-/* or 58px */
-letterSpacing: '-0.14em',
+    position: 'absolute',
+    width: '268px',
+    height: '127px',
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '72px',
+    lineHeight: '81.2%',
+    /* or 58px */
+    letterSpacing: '-0.14em',
+    fontWeight: '50px',
 
+}
+
+const homeDiv = {
+    position: 'absolute',
+    width: '111px',
+    height: '39px',
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '16px',
+    lineHeight: '81.2%',
+    /* or 58px */
+    letterSpacing: '-0.14em',
+   
 
 }
 
